04394000161de945f014b1edeec5972df4724df6
m3u8playlist <44310582+m3u8playlist@users.noreply.github.com>
2022-01-12 14:06:35 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] != "替换成你的用户名" || $_SERVER['PHP_AUTH_PW'] != "替换成你的密码") {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'sorry! you are not allowed to see this page.';
+    exit;
+} 
+
+else{
+    header('Content-Type: text/html; charset=utf-8');
+    switch($_GET['cmd']){
+        case "get_json":
+        $output = shell_exec('yt-dlp -j ' . $_GET['url']);
+        echo "$output";
+        break;
+        case "update":
+        $output = shell_exec('yt-dlp -U');
+        echo "$output";
+        break;
+        default:
+        echo "invalid argument";
+        break;  
+    }
+}
+?>
